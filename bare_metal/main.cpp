3da1112d7eec27d6a02ece5049ec5ba2362d0daa
Simon Hall <simonjhall@gmail.com>
2013-05-18 14:17:06 +0100

    elf entry point

--- bare_metal/main.cpp
+++ bare_metal/main.cpp
@@ -90,7 +90,10 @@ struct Cpsr
 
 struct RfeData
 {
-	void(*m_pPc)(void);
+	union {
+		void(*m_pPc)(void);
+		void *func;
+	};
 	Cpsr m_cpsr;
 	unsigned int *m_pSp;
 };
@@ -232,9 +235,6 @@ static inline void SetupMmu(unsigned int physEntryPoint)
     for (unsigned int i = physEntryPoint >> 20; i < (physEntryPoint + image_length_section_align) >> 20; i++)
     	pEntries[i].fault.Init();
 
-//    MapPhysToVirt((void *)(PhysPages::s_startAddr + 0), 0, 0x10000, TranslationTable::kRwNa, TranslationTable::kExec, TranslationTable::kOuterInnerWbWa, 0);
-//    MapPhysToVirt((void *)(PhysPages::s_startAddr + 0x10000), (void *)(PhysPages::s_startAddr + 0x10000), end - (PhysPages::s_startAddr + 0x10000), TranslationTable::kRwRw, TranslationTable::kExec, TranslationTable::kOuterInnerWbWa, 0);
-
     SetHighBrk((void *)virt_end);
 
     //executable top section
@@ -245,45 +245,20 @@ static inline void SetupMmu(unsigned int physEntryPoint)
     			TranslationTable::kRwRw, TranslationTable::kNoExec, TranslationTable::kOuterInnerWbWa, 0);
     //IO sections
 #ifdef PBES
-    MapPhysToVirt((void *)(1152 * 1048576), (void *)(1152 * 1048576), 1048576, TranslationTable::kRwRw, TranslationTable::kNoExec, TranslationTable::kShareableDevice, 0);
+    MapPhysToVirt((void *)(1152 * 1048576), (void *)(1152 * 1048576), 1048576,
+    		TranslationTable::kRwRw, TranslationTable::kNoExec, TranslationTable::kShareableDevice, 0);
 #else
-    VirtMem::MapPhysToVirt((void *)(257 * 1048576), (void *)(257 * 1048576), 1048576, TranslationTable::kRwRw, TranslationTable::kNoExec, TranslationTable::kShareableDevice, 0);
+    VirtMem::MapPhysToVirt((void *)(257 * 1048576), (void *)(257 * 1048576), 1048576,
+    		TranslationTable::kRwRw, TranslationTable::kNoExec, TranslationTable::kShareableDevice, 0);
 #endif
 
     //map the trampoline vector one page up from exception vector
-    VirtMem::MapPhysToVirt(PhysPages::FindPage(), VectorTable::GetTableAddress(), 4096, TranslationTable::kRwRo, TranslationTable::kExec, TranslationTable::kShareableDevice, 0);
+    VirtMem::MapPhysToVirt(PhysPages::FindPage(), VectorTable::GetTableAddress(), 4096,
+    		TranslationTable::kRwRo, TranslationTable::kExec, TranslationTable::kOuterInnerWbWa, 0);
 
     unsigned int tramp_phys = (unsigned int)&__trampoline_start__ - virt_phys_offset;
-    VirtMem::MapPhysToVirt((void *)tramp_phys, (void *)((unsigned int)VectorTable::GetTableAddress() + 0x1000), 4096, TranslationTable::kRwRo, TranslationTable::kExec, TranslationTable::kShareableDevice, 0);
-
-    for (unsigned int count = 0; count < 5; count++)
-    {
-    	PrinterUart<PL011> p;
-    	p.Print(count * 1048576);
-    	p.PrintString(": ");
-
-    	if (pEntries[count].Print(p))
-    	{
-    		p.PrintString("\r\n");
-    		TranslationTable::TableEntryL2 *pL2Phys = pEntries[count].pageTable.GetPhysPageTable();
-    		TranslationTable::TableEntryL2 *pL2Virt;
-
-    		if (!VirtMem::PhysToVirt(pL2Phys, &pL2Virt))
-    			p.PrintString("\tNO PHYS->VIRT MAPPING FOR PAGE TABLE\r\n");
-    		else
-    		{
-				for (unsigned int inner = 0; inner < 256; inner++)
-				{
-					p.PrintString("\t");
-					p.Print(count * 1048576 + inner * 4096);
-					p.PrintString(": ");
-					pL2Virt[inner].Print(p);
-				}
-
-				p.PrintString("\r\n");
-    		}
-    	}
-    }
+    VirtMem::MapPhysToVirt((void *)tramp_phys, (void *)((unsigned int)VectorTable::GetTableAddress() + 0x1000), 4096,
+    		TranslationTable::kRwRo, TranslationTable::kExec, TranslationTable::kOuterInnerWbWa, 0);
 
     //copy in the high code
     memcpy((unsigned char *)(0xffff0fe0 - 4), &TlsLow, (unsigned int)TlsHigh - (unsigned int)TlsLow);
@@ -315,6 +290,10 @@ extern "C" void Setup(unsigned int entryPoint)
 	SetupMmu(entryPoint);
 
 	PL011::EnableFifo(false);
+	PL011::EnableUart(true);
+
+	VirtMem::DumpVirtToPhys(0, (void *)0xffffffff, true, true);
+
 	PrinterUart<PL011> p;
 	p.PrintString("pre-mmu\r\n");
 
@@ -342,13 +321,16 @@ extern "C" void Setup(unsigned int entryPoint)
 	startingElf.Load(&_binary__home_simon_workspace_tester_Debug_tester_strip_start,
 			(unsigned int)&_binary__home_simon_workspace_tester_Debug_tester_strip_size);
 
+	bool has_tls = false;
+	unsigned int tls_memsize, tls_filesize, tls_vaddr;
+
 	for (unsigned int count = 0; count < startingElf.GetNumProgramHeaders(); count++)
 	{
 		void *pData;
 		unsigned int vaddr;
 		unsigned int memSize, fileSize;
 
-		startingElf.GetProgramHeader(count, &pData, &vaddr, &memSize, &fileSize);
+		int p_type = startingElf.GetProgramHeader(count, &pData, &vaddr, &memSize, &fileSize);
 
 		p.PrintString("Header ");
 		p.Print(count);
@@ -361,10 +343,52 @@ extern "C" void Setup(unsigned int entryPoint)
 		p.PrintString(" file size ");
 		p.Print(fileSize);
 		p.PrintString("\r\n");
+
+		if (p_type == PT_TLS)
+		{
+			tls_memsize = memSize;
+			tls_filesize = fileSize;
+			tls_vaddr = vaddr;
+			has_tls = true;
+		}
+
+		if (p_type == PT_LOAD)
+		{
+			unsigned int beginVirt = vaddr & ~4095;
+			unsigned int endVirt = ((vaddr + memSize) + 4095) & ~4095;
+			unsigned int pagesNeeded = (endVirt - beginVirt) >> 12;
+
+			for (unsigned int i = 0; i < pagesNeeded; i++)
+			{
+				void *pPhys = PhysPages::FindPage();
+				ASSERT(pPhys != (void *)-1);
+
+				bool ok = VirtMem::MapPhysToVirt(pPhys, (void *)beginVirt, 4096,
+						TranslationTable::kRwRw, TranslationTable::kExec, TranslationTable::kOuterInnerWbWa, 0);
+				ASSERT(ok);
+
+				//clear the page
+				memset((void *)beginVirt, 0, 4096);
+
+				//copy in the file data
+				unsigned int to_copy = fileSize > 4096 ? 4096 : fileSize;
+
+				for (unsigned int c = 0; c < (to_copy >> 2); c++)
+				{
+					((unsigned int *)beginVirt)[c] = ((unsigned int *)pData)[c];
+				}
+
+				//next page
+				beginVirt += 4096;
+				pData = (void *)((unsigned int)pData + 4096);
+				fileSize -= to_copy;
+			}
+		}
 	}
 
 	RfeData rfe;
-	rfe.m_pPc = &_start;
+//	rfe.m_pPc = &_start;
+	rfe.func = startingElf.GetEntryPoint();
 	rfe.m_pSp = (unsigned int *)0xffeffffc;		//4095MB-4b
 
 	memset(&rfe.m_cpsr, 0, 4);
@@ -398,12 +422,25 @@ extern "C" void Setup(unsigned int entryPoint)
 	pAuxVec[2].a_type = AT_NULL;
 
 	pHdr->p_align = 2;
-	pHdr->p_filesz = (unsigned int)&thread_section_mid - (unsigned int)&thread_section_begin;
-	pHdr->p_memsz = (unsigned int)&thread_section_end - (unsigned int)&thread_section_begin;
+//	pHdr->p_filesz = (unsigned int)&thread_section_mid - (unsigned int)&thread_section_begin;
+//	pHdr->p_memsz = (unsigned int)&thread_section_end - (unsigned int)&thread_section_begin;
 	pHdr->p_offset = 0;
 	pHdr->p_paddr = 0;
-	pHdr->p_vaddr = (unsigned int)&thread_section_begin;
+//	pHdr->p_vaddr = (unsigned int)&thread_section_begin;
 	pHdr->p_type = PT_TLS;
+
+	if (has_tls)
+	{
+		pHdr->p_filesz = tls_filesize;
+		pHdr->p_memsz = tls_memsize;
+		pHdr->p_vaddr = tls_vaddr;
+	}
+	else
+	{
+		pHdr->p_filesz = 0;
+		pHdr->p_memsz = 0;
+		pHdr->p_vaddr = 0;
+	}
 
 	unsigned int hdr_size = sizeof(ElfW(Phdr));
 
