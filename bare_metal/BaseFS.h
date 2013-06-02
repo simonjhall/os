b586a0c94d310a18d17bdd0ee7dc40e5d875b1d8
Simon Hall <simonjhall@gmail.com>
2013-06-02 23:25:17 +0100

    more additions to file spec

--- bare_metal/BaseFS.h
+++ bare_metal/BaseFS.h
@@ -13,19 +13,6 @@
 class BaseFile
 {
 public:
-	enum Mode
-	{
-		kReadOnly,
-		kWriteOnly,
-		kReadWrite,
-	};
-	enum Whence
-	{
-		kSeekSet,
-		kSeekCur,
-		kSeekEnd,
-	};
-
 	virtual ssize_t Read(void *pBuf, size_t count);
 	virtual ssize_t Write(const void *pBuf, size_t count);
 	virtual ssize_t Writev(const struct iovec *iov, int iovcnt);
@@ -34,7 +21,11 @@ public:
 	virtual off_t Lseek(off_t offset, Whence w);
 
 	virtual void *Mmap(void *addr, size_t length, int prot, int flags, off_t offset);
+	virtual void *Mmap2(void *addr, size_t length, int prot,
+                    int flags, off_t pgoffset);
 	virtual int Munmap(void *addr, size_t length);
+
+	virtual bool Fstat(struct stat &rBuf);
 };
 
 class WrappedFile : public BaseFile
@@ -49,9 +40,22 @@ public:
 	BaseFS();
 	virtual ~BaseFS();
 
-	virtual WrappedFile &Open(const char *pFilename, BaseFile::Mode mode);
+	virtual WrappedFile &Open(const char *pFilename, unsigned int flags);
 	virtual bool Close(WrappedFile &);
 	virtual WrappedFile &Dup(WrappedFile &);
+
+	virtual bool Stat(const char *pFilename, struct stat &rBuf);
+	virtual bool Lstat(const char *pFilename, struct stat &rBuf);
+
+	virtual bool Attach(BaseFS &, const char *pTarget);
+	virtual bool Detach(const char *pTarget);
+};
+
+//maybe - chdir also needs to be considered
+class ProcessRootFS
+{
+	ProcessRootFS(BaseFS, const char *pRootFilename);
+	virtual ~ProcessRootFS();
 };
 
 #endif /* BASEFS_H_ */
